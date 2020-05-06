d92fdb0f62d099e705f4ca6d629789fb8e2a4f76
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-06 17:51:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(line)
     puts "There is nobody waiting to be served!"
   else
     serving = line.shift
-    puts "Currently serving #{line.shift}."
+    puts "Currently serving #{serving}."
   end
 end
     
