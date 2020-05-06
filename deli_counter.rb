1bf596dfb83f7c55ac9d6ec6cb5db15cb60ade9d
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-06 17:27:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,12 @@ def line(line)
   if line.length == 0
     puts "The line is currently empty."
   else
-    counter = 1
     string = "The line is currently:"
-    line.each do |name|
-      string << " #{counter}. #{name}"
-      counter += 1
+    line.each_with_index do |name, index|
+      string << " #{index + 1}. #{name}"
     end
     puts string
   end
 end
+
+take_a_number(line, name)
